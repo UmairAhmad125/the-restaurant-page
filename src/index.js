edef71f956d47d595b78c0d6c0041125b1af93c6
umairahmad125 <umairahmad125@gmail.com>
2021-01-04 17:35:11 +0500

    create bare-bones homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
+require('./css/style.css');
 console.log("hello there");
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,28 @@
 const path = require('path');
 
 module.exports = {
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 2,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+                loader: "url-loader",
+                options: {
+                    limit: 8192,
+                },
+            },
+        ],
+    },
     entry: './src/index.js',
     output: {
         filename: 'main.js',
